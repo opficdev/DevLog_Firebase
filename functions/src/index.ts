bf2e04f3cf4ddd9a61432986b8c061b9aad84061
opficdev <162981733+opficdev@users.noreply.github.com>
2026-07-03 11:25:34 +0900

    refactor: rest api 형태로 변경된 functions 제거

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,20 +2,6 @@ import * as admin from "firebase-admin";
 import * as dotenv from "dotenv";
 import * as path from "path";
 
-// Apple 인증 관련 함수 가져오기
-import {
-    requestAppleCustomToken,
-    requestAppleRefreshToken,
-    refreshAppleAccessToken,
-    revokeAppleAccessToken
-} from "./auth/apple";
-
-// GitHub 인증 관련 함수 가져오기
-import {
-    requestGithubTokens,
-    revokeGithubAccessToken
-} from "./auth/github";
-
 // import {
 
 // } from "./auth/google";
@@ -46,26 +32,11 @@ import {
 } from "./todoCategory/update";
 
 import {
-    requestTodoDeletion,
-    undoTodoDeletion
-} from "./todo/deletion";
-
-import {
-    requestPushNotificationDeletion,
-    undoPushNotificationDeletion
-} from "./notification/deletion";
-
-import {
     removeTodoNotificationDocuments,
     removeCompletedTodoNotificationRecords,
     cleanupNotificationDispatches,
     cleanupSoftDeletedNotifications
 } from "./notification/cleanup";
-
-import {
-    requestWebPageDeletion,
-    undoWebPageDeletion
-} from "./webPage/deletion";
 
 import {
     cleanupSoftDeletedWebPages
@@ -89,20 +60,6 @@ dotenv.config({
 // Firebase 앱 초기화
 admin.initializeApp();
 
-// Apple 인증 함수들 내보내기
-export { 
-    requestAppleCustomToken,
-    requestAppleRefreshToken,
-    refreshAppleAccessToken,
-    revokeAppleAccessToken
-};
-
-// GitHub 인증 함수들 내보내기
-export {
-    requestGithubTokens,
-    revokeGithubAccessToken
-};
-
 // Google 인증 함수들 (나중에 구현되면 추가)
 
 export {
@@ -123,13 +80,7 @@ export {
     syncTodoNotificationCategory,
     requestMoveRemovedCategoryTodosToEtc,
     completeMoveRemovedCategoryTodosToEtc,
-    requestTodoDeletion,
-    undoTodoDeletion,
-    requestPushNotificationDeletion,
-    undoPushNotificationDeletion,
     cleanupSoftDeletedNotifications,
-    requestWebPageDeletion,
-    undoWebPageDeletion,
     cleanupSoftDeletedWebPages,
     prodApi,
     stagingApi
