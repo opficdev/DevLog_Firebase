95cb945b85d98163fa855cc7795e68b5a96a44b6
opficdev <162981733+opficdev@users.noreply.github.com>
2026-08-03 14:50:11 +0900

    feat: FirebaseModule을 애플리케이션 부팅에 연결

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { FirebaseModule } from './firebase/firebase.module';
+
 /** 애플리케이션의 최상위 의존성 경계를 구성하는 모듈입니다. */
-@Module({})
+@Module({
+  imports: [FirebaseModule],
+})
 export class AppModule {}
